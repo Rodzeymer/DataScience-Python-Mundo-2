c31764d2adedd91e381ffafc584ba542b9d4176e
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-15 16:54:32 -0300

    Ficou pra amanhã

---
+++ Python Mundo 2 - Desafio 36.py
@@ -0,0 +1,14 @@
+renda = float(input('Qual a renda do comprador?'))
+
+precoCasa = float(input('Qual o valor da casa que quer comprar?'))
+
+tempoFinanciamento = int(input('Deseja pagar a casa em quantos anos?'))
+
+valorPrestacao = precoCasa / (tempoFinanciamento*12)
+
+print(renda)
+print(precoCasa)
+print(tempoFinanciamento)
+print(tempoFinanciamento*12)
+print(valorPrestacao)
+
