fa817c3a5b57f93f4c3ca31fb16a7ded28d3179f
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-16 14:24:44 -0300

    36 finalizado

--- Python Mundo 2 - Desafio 36.py
+++ Python Mundo 2 - Desafio 36.py
@@ -6,9 +6,7 @@ tempoFinanciamento = int(input('Deseja pagar a casa em quantos anos?'))
 
 valorPrestacao = precoCasa / (tempoFinanciamento*12)
 
-print(renda)
-print(precoCasa)
-print(tempoFinanciamento)
-print(tempoFinanciamento*12)
-print(valorPrestacao)
-
+if valorPrestacao > renda*0.3:
+	print('O financiamento ficaria em {} vezes de R${:.2f}, acima do recomendado para sua renda que é de R${:.2f}, correspondendo a 30% dela.'.format(tempoFinanciamento*12, valorPrestacao, renda*0.3))
+else: 
+	print('O seu orçamento comporta a parcela de R${:.2f} por {} meses, pois fica abaixo dos 30% de seu orçamento, R${:.2f}'.format(valorPrestacao, tempoFinanciamento*12, renda*0.3))
