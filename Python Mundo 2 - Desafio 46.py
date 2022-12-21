0de02530f96ed0f15d94cae645ce98e104a3e2ff
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-21 14:39:44 -0300

    46 finalizado

---
+++ Python Mundo 2 - Desafio 46.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+comecar = input('Aperte enter para começar a contagem dos fogos')
+
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)
+print('Fogos explodindo')
+sleep(0.5)
+print('Barulho ensurdecedor')
+sleep(0.5)
+print('Cachorro latindo')
+sleep(0.5)
+print('Criança chorando')
+sleep(0.5)
+print('Feliz ano novo')
+sleep(0.5)
+print('Gente se abraçando')
+sleep(0.5)
+print('Silêncio finalmente')
