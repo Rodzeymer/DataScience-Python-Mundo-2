7e1c080f3c02c226b8bc7f5203f2388cd56c1ebb
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-21 14:51:49 -0300

    47 finalizado

---
+++ Python Mundo 2 - Desafio 47.py
@@ -0,0 +1,7 @@
+pares = []
+for c in range (1, 50):
+    if c %2 ==0:
+        print('O número {} é par'.format(c))
+        pares.append(c)
+print('Os números pares entre 1 e 50 são {}'.format(pares))
+        
