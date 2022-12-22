4122bfd80baddccef15a34489795c95951ecb1ab
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-22 16:54:48 -0300

    Bora

--- bora.py
+++ bora.py
@@ -1,2 +1,2 @@
-https://www.cursoemvideo.com/curso/python-3-mundo-2/aulas/repeticoes-em-python-for/modulos/estrutura-de-repeticao-for-2/
-32.30
+https://www.cursoemvideo.com/curso/python-3-mundo-2/aulas/repeticoes-em-python-while/modulos/aula-14-estrutura-de-repeticao-while/
+34.00
