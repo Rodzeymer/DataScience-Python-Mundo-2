0fde74a3978986b62f6eb9be0af662fc8c56c5f6
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-22 15:22:49 -0300

    62 finalizado

--- Python Mundo 2 - Desafio 61.py
+++ Python Mundo 2 - Desafio 61.py
@@ -1,13 +1,18 @@
 primeiroTermo = int(input('Digite o primeiro termo da progressão'))
 razao = int(input('Agora digite o valor da razão'))
-termo = 10
-decimoTermo = primeiroTermo + (10-1)* razao
-print('''Progressão aritmética
-    Primeiro termo {}
-    Razão {}'''.format(primeiroTermo, razao))
-contador = 0
-while contador <termo:
-    progressao = primeiroTermo+(razao*contador)
-    contador = contador +1
-    print('{} '.format(progressao), end='-> ')
-print('FIM')
+termos = int(input('Digite quantos termos deseja calcular'))-1
+maisTermos = 1
+while maisTermos !=0:
+    termos = termos+maisTermos
+    print('''Progressão aritmética
+        Primeiro termo {}
+        Razão {}
+        Termos {}'''.format(primeiroTermo, razao, termos))
+    contador = 0
+    while contador <termos:
+        progressao = primeiroTermo+(razao*contador)
+        contador = contador +1
+        print('{} '.format(progressao), end='-> ')
+    print('FIM')
+    maisTermos = int(input('Quer calcular mais termo?'))
+print('Aplicativo encerrado!')
