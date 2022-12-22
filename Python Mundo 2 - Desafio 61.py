2d6ee9cf74b44c9f15fd171c005aade526963e75
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-22 15:26:38 -0300

    61 recuperado

---
+++ Python Mundo 2 - Desafio 61.py
@@ -0,0 +1,20 @@
+primeiroTermo = int(input('Digite o primeiro termo da progressão'))
+razao = int(input('Agora digite o valor da razão'))
+termo = 10
+decimoTermo = primeiroTermo + (10-1)* razao
+print('''Progressão aritmética
+    Primeiro termo {}
+    Razão {}'''.format(primeiroTermo, razao))
+contador = 0
+while contador <termo:
+    progressao = primeiroTermo+(razao*contador)
+    contador = contador +1
+    print('{} '.format(progressao), end='-> ')
+print('FIM')
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
