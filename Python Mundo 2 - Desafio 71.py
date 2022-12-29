f2291310b64c1a957377fd9200c4accbf3fb8810
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-12-29 20:02:24 -0300

    71 refeito

--- Python Mundo 2 - Desafio 71.py
+++ Python Mundo 2 - Desafio 71.py
@@ -1,19 +1,19 @@
 saldoInicial = int(input('Quanto deseja sacar?'))
 saldo = saldoInicial
 
+print('Total de notas: ')
 saque50 = saldo//50
 saldo = saldo - (saque50*50)
+if saque50>0:
+    print(f'Notas de R$50: {saque50}')
 saque20 = saldo//20
 saldo = saldo - (saque20*20)
+if saque20>0:
+    print(f'Notas de R$20: {saque20}')
 saque10 = saldo//10
 saldo = saldo - (saque10*10)
+if saque10>0:
+    print(f'Notas de R10: {saque10}')
 saque1 = saldo
-
-print(f'''Total de notas:
-Notas de R$50: {saque50}
-Notas de R$20: {saque20}
-Notas de R$10: {saque10}
-Notas de R$1: {saque1}
-
-
-''')
+if saque1>0:
+    print(f'Notas de R$1: {saque1}')
