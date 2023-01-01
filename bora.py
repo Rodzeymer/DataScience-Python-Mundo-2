5e229b9cc294e6b2c5b95262a7a4bf3b67a8c128
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-01 19:57:19 -0300

    bora

--- bora.py
+++ bora.py
@@ -1,6 +1,4 @@
-https://www.cursoemvideo.com/curso/python-3-mundo-2/aulas/repeticoes-em-python-while/modulos/aula-15-interrompendo-repeticoes-while/
 
-32.15
 
 https://www.youtube.com/watch?v=5b9Z8toVaAU
 
