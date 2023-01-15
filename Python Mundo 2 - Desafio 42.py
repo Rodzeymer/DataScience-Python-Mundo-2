3cba43abec9f79a3eaa45c9135c708b2a22c2301
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-15 15:43:48 -0300

    desafios 42 e 43 refeitos

--- Python Mundo 2 - Desafio 42.py
+++ Python Mundo 2 - Desafio 42.py
@@ -1,15 +1,27 @@
+# Seguindo o aumento da complexidade das calculadoras, agora o sistema irá perguntar as medidas dos 
+# lados de um triângulo, e, em sendo possível a construção desse triângulo irá determinar que tipo de 
+# triângulo ele é: isóceles, equilátero, ou escaleno.
+
+print('*' *30)
+print("{'Python Mundo 2 - Desafio 42':^30}")
+print('*' *30) 
+
 ladoA = int(input('Digite o tamanho do lado A'))
 ladoB = int(input('Digite o tamanho do lado B'))
 ladoC = int(input('Digite o tamanho do lado C'))
 
 if ladoA + ladoB >= ladoC and ladoA + ladoC >= ladoB and ladoB + ladoC >= ladoA:
-    print('Há um triangulo possível com os lados {}, {} e {}'.format(ladoA, ladoB, ladoC))
+    print(f'Há um triangulo possível com os lados {ladoA}, {ladoB} e {ladoC}')
     if ladoA == ladoB and ladoB ==ladoC:
         tipoTriangulo = 'Equilátero'
     elif ladoA == ladoB or ladoB == ladoC or ladoC ==ladoA:
         tipoTriangulo = 'Isóceles'
     elif ladoA != ladoB and ladoB != ladoC and ladoA != ladoC:
         tipoTriangulo = 'Escaleno'
-    print('E esse triângulo é do tipo: {}'.format(tipoTriangulo))
+    print(f'E esse triângulo é do tipo: {tipoTriangulo}')
 else:
-    print('Não há um triangulo possível com os lados {}, {} e {}'.format(ladoA, ladoB, ladoC))
+    print(f'Não há um triangulo possível com os lados {ladoA}, {ladoB} e {ladoC}')
+    
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
--- Python Mundo 2 - Desafio 43.py
+++ Python Mundo 2 - Desafio 43.py
@@ -1,7 +1,15 @@
-altura = float(input('Digite a sua altura, em metros'))
+# Seguindo em calculadoras clássicas, vamos para a calculadora de IMC, neste programa o usuário informa
+# a sua altura em centimetros e seu peso em metros, então o sistema calcula o seu IMC e responde com a 
+# categoria, entre abaixo do peso, ideal, sobrepeso e obeso.
+
+print('*' *30)
+print("{'Python Mundo 2 - Desafio 43':^30}")
+print('*' *30) 
+
+altura = float(input('Digite a sua altura, em centimetros'))
 peso = float(input('Agora digite o seu peso, em quilos'))
 
-imc  = peso/(altura**2)
+imc  = peso/((altura/100)**2)
 if imc< 18.5:
     classeImc = ('Abaixo do peso')
 elif imc >=18.5 and imc <25:
@@ -13,4 +21,7 @@ elif imc>=30 and imc<40:
 elif imc>40:
     classeImc = ('Obesidade Mórbida')
 
-print('Com altura de {} metros e peso de {}kg, seu IMC é {:.2f} estando classificado como: {}'.format(altura, peso, imc, classeImc))
+print(f'Com altura de {(altura/100)} metros e peso de {peso}kg, seu IMC é {imc :.2f} estando classificado como: {classeImc}')
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
