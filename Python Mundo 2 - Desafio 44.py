8e450a0792f6c5c95547abf6ec6b5b244fbc1cb4
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-15 16:20:53 -0300

    Desafios 44 e 45 refeitos

--- Python Mundo 2 - Desafio 44.py
+++ Python Mundo 2 - Desafio 44.py
@@ -1,6 +1,16 @@
+# Avançando um degrau na complexidade de calculadoras, temos essa que foi construída para calcular, dado
+# o valor do produto, o preço final conforme o método de pagamento escolhido, o usuário insere o valor e 
+# o sistema oferece 4 métodos de pagamento, à vista, pix ou dividir no cartão, cada qual com seu desconto
+# ou juros, exibindo ao final qual foi o método, valor original e final do produto e, se parcelado, quantas
+# parcelas e seus valores.
+
+print('*' *30)
+print("{'Python Mundo 2 - Desafio 44':^30}")
+print('*' *30)
+
 preco = float(input('Digite o valor do produto'))
 
-print('O preço do produto é R${}, selecione o método de pagamento'.format(preco))
+print(f'O preço do produto é R${preco}, selecione o método de pagamento')
 metodo = int(input('''
 1 - para à vista, boleto ou PIX, com desconto de 10%;
 2 - para à vista, no crédito ou débito, com desconto de 5%;
@@ -9,21 +19,24 @@ metodo = int(input('''
 
 if metodo == 1:
     valor = preco-preco*0.1
-    print('À vista, boleto ou PIX, o valor final do produto fica por R${:.2f}, já com o desconto aplicado'.format(valor))
+    print(f'À vista, boleto ou PIX, o produto vendido por {preco} fica com o valor final de R${valor :.2f}, com o desconto aplicado')
 elif metodo == 2:
     valor = preco-preco*0.05
-    print('À vista, no crédito ou débito, o valor final do produto fica por R${:.2f}, já com o desconto aplicado'.format(valor))
+    print(f'À vista, no crédito ou débito, o valor final do produto fica por R${valor :.2f}, já com o desconto de R${preco-valor} aplicado')
 elif metodo == 3:
     valor = preco
     parcela = valor/2
-    print('Dividindo sua compra em 2x no cartão de crédito, resulta em 2 parcelas de R${:.2f}, resultando no total de R${:.2f}'.format(parcela, valor))
+    print(f'Dividindo sua compra em 2x no cartão de crédito, resulta em 2 parcelas de R${parcela :.2f}, resultando no total de R${valor :.2f}')
 elif metodo == 4:
     valor = preco+preco*0.2
-    numParcelas = int(input('Digite em quantas parcelas quer dividir o valor de R${:.2f}'.format(valor)))
+    numParcelas = int(input(f'Digite em quantas parcelas quer dividir o valor de R${valor :.2f}'))
     parcela = valor / numParcelas
-    print('Dividindo a compra de R${:.2f} em {} parcelas, com juros de 20%, resulta em compra final de R${:.2f}, com {} parcelas de R${:.2f}'
-    .format(preco, numParcelas, valor, numParcelas, parcela))
+    print(f'Dividindo a compra de R${preco :.2f} em {numParcelas} parcelas, com juros de 20%, resulta em compra final de R${valor:.2f}, com {numParcelas} parcelas de R${parcela:.2f}')
 else:
     print('Opção inválida')
     
 print('Obrigado pela preferência!')
+
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
--- Python Mundo 2 - Desafio 45.py
+++ Python Mundo 2 - Desafio 45.py
@@ -1,3 +1,12 @@
+# Neste desafio foi construido um algoritmo em que o usuário joga jokenpo, ou pedra, papel ou tesoura, 
+# contra o computador. O usuário faz sua escolha, 1-Pedra, 2-Papel ou 3-Tesoura, o sistema escolhe usando
+# o a função choice do módulo random de um array com as mesmas opções, o programa compara e exibe o resultado
+#Q entre vitória do jogador, vitória do PC ou empate.
+
+print('*' *30)
+print("{'Python Mundo 2 - Desafio 45':^30}")
+print('*' *30)
+
 from random import choice
 
 escolhaJogador = int(input('''Escolha entre:
@@ -6,31 +15,35 @@ escolhaJogador = int(input('''Escolha entre:
 3-Tesoura'''))
 
 if escolhaJogador>0 and escolhaJogador<4:
-
     escolhas = ['Pedra', 'Papel', 'Tesoura']
     escolhaJogador = escolhas[escolhaJogador-1]
     escolhaPC = choice(escolhas)
 
     if escolhaJogador == escolhaPC:
-        print('EMPATE, o Jogador escolheu {} e o PC escolheu {}'.format(escolhaJogador, escolhaPC))
-
+        print(f'EMPATE, o Jogador escolheu {escolhaJogador} e o PC escolheu {escolhaPC}')
+        
     elif escolhaJogador == 'Pedra':
         if escolhaPC =='Papel':
-            print('PC ganhou, Jogador escolhe {} e PC escolheu {}'.format(escolhaJogador, escolhaPC))
+            print(f'PC ganhou, Jogador escolhe {escolhaJogador} e PC escolheu {escolhaPC}')
         elif escolhaPC =='Tesoura':
-            print('Jogador ganhou, Jogador escolhe {} e PC escolheu {}'.format(escolhaJogador, escolhaPC))
-
+            print(f'Jogador ganhou, Jogador escolhe {escolhaJogador} e PC escolheu {escolhaPC}')
+   
     elif escolhaJogador == 'Papel':
         if escolhaPC =='Tesoura':
-            print('PC ganhou, Jogador escolhe {} e PC escolheu {}'.format(escolhaJogador, escolhaPC))
+            print(f'PC ganhou, Jogador escolhe {escolhaJogador} e PC escolheu {escolhaPC}')
+        
         elif escolhaPC =='Pedra':
-            print('Jogador ganhou, Jogador escolhe {} e PC escolheu {}'.format(escolhaJogador, escolhaPC))
-
+            print(f'Jogador ganhou, Jogador escolhe {escolhaJogador} e PC escolheu {escolhaPC}')
+    
     elif escolhaJogador == 'Tesoura':
         if escolhaPC =='Pedra':
-            print('PC ganhou, Jogador escolhe {} e PC escolheu {}'.format(escolhaJogador, escolhaPC))
+            print(f'PC ganhou, Jogador escolhe {escolhaJogador} e PC escolheu {escolhaPC}')
+        
         elif escolhaPC =='Papel':
-            print('Jogador ganhou, Jogador escolhe {} e PC escolheu {}'.format(escolhaJogador, escolhaPC))
-
+            print(f'Jogador ganhou, Jogador escolhe {escolhaJogador} e PC escolheu {escolhaPC}')
 else:
     print('Opção inválida')
+    
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
