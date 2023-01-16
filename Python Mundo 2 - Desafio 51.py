44a6121fd01d4ef4825b08f22523639de01b9ac4
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-16 09:06:20 -0300

    Desafios 51 e 52 refeitos, melhorados e corrigidos!

--- Python Mundo 2 - Desafio 51.py
+++ Python Mundo 2 - Desafio 51.py
@@ -1,10 +1,21 @@
+# Vamos ver uma calculadora que retorna os 10 primeiros termos de uma progressão aritmética 
+# baseado no primeiro termo e no valor da razão informados pelo usuário 
+
+print('*' *30)
+print(f"{'Python Mundo 2 - Desafio 51':^30}")
+print('*' *30)
+
 primeiroTermo = int(input('Digite o primeiro termo da progressão'))
 razao = int(input('Agora digite o valor da razão'))
 termo = 10
 decimoTermo = primeiroTermo + (10-1)* razao
-print('''Progressão aritmética
-    Primeiro termo {}
-    Razão {}'''.format(primeiroTermo, razao))
+print(f'''Progressão aritmética
+    Primeiro termo {primeiroTermo}
+    Razão {razao}''')
 for c in range(primeiroTermo, decimoTermo + razao, razao):
-    print('{} '.format(c), end='-> ')
+    print(f'{c} ', end='-> ')
+    
 print('FIM')
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
--- Python Mundo 2 - Desafio 52.py
+++ Python Mundo 2 - Desafio 52.py
@@ -1,3 +1,13 @@
+# Neste desafio foi construído um algoritmo que identifica se o número informado pelo usuário é
+# ou não primo, já que um número primo possui apenas dois divisores de resto zero, o 1 e ele
+# mesmo, o programa verifica quantos divisores que retornam 0 há entre 1 e ele, se houver
+# apenas 2 divisores então o número é primo, ainda são retornados os divisores do número, 
+# primo ou não
+
+print('*' *30)
+print(f"{'Python Mundo 2 - Desafio 52':^30}")
+print('*' *30)
+
 numero = int(input('Digite um número inteiro'))
 
 divisoes = 0
@@ -8,6 +18,11 @@ for c in range (1, numero+1):
         divisores.append(c)
         
 if divisoes == 2:
-    print('O número {} possui 2 divisões, é divisível por 1 e por ele mesmo, logo é um número primo'.format(numero))
+    print(f'O número {numero} possui 2 divisões, é divisível por 1 e por ele mesmo, logo é um número primo')
 elif divisoes > 2:
-    print('O número {} possui {} divisões, é divisível por {}, não sendo primo'.format(numero, divisoes, divisores))
+    print(f'O número {numero} possui {divisoes} divisões, é divisível por {divisores}, não sendo primo')
+    
+print('FIM')
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
