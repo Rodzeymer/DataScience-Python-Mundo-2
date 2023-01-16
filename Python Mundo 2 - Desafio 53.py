dd38b6535e8bff5cbc29fd6d7849bc1516cb2afa
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-16 09:21:18 -0300

    Desafios 53 e 54 refeitos, melhorados e corrigidos!

--- Python Mundo 2 - Desafio 53.py
+++ Python Mundo 2 - Desafio 53.py
@@ -1,10 +1,24 @@
+# Agora iremos manipular uma string e identificar se o que o usuário digitou é um palindromo, 
+# conceito em que uma palavra ou frase pode ser lida da mesma forma de trás pra frente,
+# desconsiderando-se espaços e acentuações, para isso removemos todos os espaços e invertemos
+# a string, armazenando-a em outra variável, se a original e a reversa forem iguais então
+#temos um palindromo
+
+print('*' *30)
+print(f"{'Python Mundo 2 - Desafio 53':^30}")
+print('*' *30)
+
 frase = str(input('Digite uma frase')).lower()
 
 fraseSemEspacos = frase.replace(' ', '')
-
 fraseOposta = fraseSemEspacos[::-1]
 
 if fraseSemEspacos == fraseOposta:
-    print('{} é um palindromo ficando {} quando invertida'.format(frase, fraseOposta))
+    print(f'{frase} é um palindromo ficando {fraseOposta} quando invertida')
 else:
-    print('{} não é um palíndromo, ficando {} quando invertida'.format(frase, fraseOposta))
+    print(f'{frase} não é um palíndromo, ficando {fraseOposta} quando invertida')
+
+print('FIM')
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
--- Python Mundo 2 - Desafio 54.py
+++ Python Mundo 2 - Desafio 54.py
@@ -1,3 +1,14 @@
+# O desafio agora é receber o ano de nascimento de 7 pessoas, identificar quem tem mais de 
+# 21 anos e identificar quanto possuem 21 anos ou mais, que são menores de idade e quais são
+# as suas idades, depois de pegar o ano atual do sistema, fiz um loop pedindo a idade de 7 
+# pessoas, passando por um if que verifica a idade e adiciona em um vetor os maiores de 21
+# e em outro os menores e ao final exibe as mensagens de quantas pessoas possuem 21 anos ou mais
+# e menores e as idades dos dois grupos
+
+print('*' *30)
+print(f"{'Python Mundo 2 - Desafio 54':^30}")
+print('*' *30)
+
 from datetime import date
 
 anoAtual = date.today().year
@@ -6,7 +17,7 @@ menorIdade = 0
 maiores =[]
 menores = []
 for contador in range(1,8):
-    anoNascimento = int(input('Digite o ano de nascimento da pessoa {}'.format(contador)))
+    anoNascimento = int(input(f'Digite o ano de nascimento da pessoa {contador}'))
     idade = anoAtual-anoNascimento
     if idade >=21:
         maiorIdade = maiorIdade +1
@@ -14,6 +25,10 @@ for contador in range(1,8):
     else:
         menorIdade = menorIdade+1
         menores.append(idade)
-print('Dentre as 7 pessoas, {} atingiram a maioridade e {} ainda são menor de idade'.format(maiorIdade, menorIdade))
-print('As pessoas de maior idade possuem idades de {}'.format(maiores))
-print('As pessoas de menor idade possuem idades de {}'.format(menores))
+print(f'Dentre as 7 pessoas, {maiorIdade} atingiram a maioridade e {menorIdade} ainda são menor de idade')
+print(f'As pessoas de maior idade possuem idades de {maiores}')
+print(f'As pessoas de menor idade possuem idades de {menores}')
+
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
