b70a9660374ff78562c925c1668c05d0b32810cf
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-16 10:38:58 -0300

    Desafios 55 e 56 refeitos, melhorados e corrigidos!

--- Python Mundo 2 - Desafio 55.py
+++ Python Mundo 2 - Desafio 55.py
@@ -1,11 +1,24 @@
+# Semelhante ao desafio passado, mas agora vamos classificar o peso, o usuário informa o peso
+# de 5 pessoas e o sistema irá retornar, dentre eles, o maior e o menor peso, só que dessa 
+# vez o código não apresenta "macetes" para funcionar de forma adequada
+
+print('*' *30)
+print(f"{'Python Mundo 2 - Desafio 55':^30}")
+print('*' *30)
+
 pesos = []
 for contador in range(1,6):
-    peso = float(input('Digite o peso da pessoa {}'.format(contador)))
+    peso = float(input(f'Digite o peso da pessoa {contador}'))
     pesos.append(peso)
     
 pesosOrdenados = sorted(pesos)
 
 maiorPeso = pesosOrdenados[len(pesosOrdenados)-1]
 menorPeso = pesosOrdenados[0]
-print(maiorPeso)
-print(menorPeso)
+
+print(f'O maior peso registrado foi o de {maiorPeso}Kg')
+print(f'O menor peso registrado foi o de {menorPeso}Kg')
+
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
--- Python Mundo 2 - Desafio 56.py
+++ Python Mundo 2 - Desafio 56.py
@@ -1,3 +1,15 @@
+# Neste desafio o algoritmo deve, num grupo de 4 pessoas a serem informadas pelo usuário, 
+# registrar a idade de todos, retornando a média; identificar o homem mais velho do grupo e 
+# informar quantas mulheres tem menos de 20 anos. Para isso iremos colocar num loop de 4
+# iterações os pedidos para o usuário informar os nomes, idades e sexo dos 4 integrantes, 
+# em que vão ficar registrados, em vetores diferentes, os nomes, pesos e sexos, para então
+# identificar o homem de maior idade, e realizar a soma das mulheres com mais de 20 anos e 
+# ao final realizar a média de todas as idades informadas
+
+print('*' *30)
+print(f"{'Python Mundo 2 - Desafio 55':^30}")
+print('*' *30)
+
 nomes = []
 idades = []
 sexos = []
@@ -6,12 +18,12 @@ mulherMenor =0
     
 for contador in range (1,5):
     print('<<{}ª Pessoa>>'.format(contador))
-    nome = str(input('Digite o nome da pessoa {}'.format(contador))).strip()
+    nome = str(input(f'Digite o nome da pessoa {contador}')).strip()
     nomes.append(nome)
-    idade = int(input('Digite a idade de {}'.format(nome)))
+    idade = int(input(f'Digite a idade de {nome}'))
     idades.append(idade)
     somaIdades = somaIdades+idade
-    sexo = str(input('Digite o sexo de {}, [M/F]'.format(nome))).strip()
+    sexo = str(input(f'Digite o sexo de {nome}, [M/F]')).strip()
     sexos.append(sexo)
     if sexo in'Mm' and contador == 1:
         maiorIdade = idade
@@ -23,6 +35,13 @@ for contador in range (1,5):
         mulherMenor=+1
 
 mediaIdades=somaIdades/len(idades)
-print('A média das idades dos 4 integrantes é {}'.format(mediaIdades))
-print('O homem mais velho é {}, com a idade de {} anos'.format(nomeMaisVelho, maiorIdade))
-print('O grupo possui {} mulheres com menos de 20 anos'.format(mulherMenor))
+print(f'A média das idades dos 4 integrantes é {mediaIdades}')
+if sexo not in 'Mm':
+    print('Neste grupo não há homens')
+elif sexo in 'Mm':
+    print(f'O homem mais velho é {nomeMaisVelho}, com a idade de {maiorIdade} anos')
+print(f'O grupo possui {mulherMenor} mulheres com menos de 20 anos')
+
+print('*' *30)
+print(f"{'FIM':^30}")
+print('*' *30)
