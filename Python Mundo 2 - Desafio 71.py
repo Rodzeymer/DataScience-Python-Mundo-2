956d69a929fb22cf7df8d0292481e700d918793d
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2023-01-17 09:08:21 -0300

    Deafio 71 refeito, melhorado e corrigido!

--- Python Mundo 2 - Desafio 71.py
+++ Python Mundo 2 - Desafio 71.py
@@ -1,7 +1,13 @@
 # Para encerrar esse Mundo 2 o boss se apresenta na forma de um algoritmo que simula
-# um caixa eletrônico
+# um caixa eletrônico, em que o usuário solicita o saque de um valor inteiro qualquer e 
+# o sistema responde quantas notas de 50, 20, 10, e 1 real serão dadas ao cliente.
 
-# <><><><><><>< TERMINAR DE COMENTAR O QUANTO ANTES <><><><><><><
+# Para isso o sistema pega o valor total e testa quantas divisões inteiras são necessárias
+# para reduzir o valor restante abaixo do valor de dada nota, de forma decrescente, em que
+# para reduzir o valor do pedido para abaixo de 50 reais, por exemplo, quantas notas, divisões, 
+# de 50 são necessárias, e passa o saldo restante do pedido para o teste do valor seguinte e 
+# exibe na tela quantas notas foram dadas daquele valor, até que o saldo restante do pedido 
+# seja menor que 10 qu é a quantidade de notas de 1 que serão dadas
 
 print('*' *30)
 print(f"{'Python Mundo 2 - Desafio 71' :^30}")
@@ -26,6 +32,7 @@ if saque10>0:
 saque1 = saldo
 if saque1>0:
     print(f'Notas de R$1: {saque1}')
+    
 print('*' *30)
 print(f"{'FIM':^30}")
 print('*' *30)
